12f6ad7b7027efc093e1969a7a8f3baf51498b24
imomryk <i.momryk@gmail.com>
2021-11-15 20:58:01 +0200

    html css done

--- main.js
+++ main.js
@@ -1,48 +1,48 @@
-console.log()
+// console.log()
 
-document.querySelector(".slide").addEventListener('touchstart', handleTouchStart, false);        
-document.querySelector(".slide").addEventListener('touchmove', handleTouchMove, false);
+// document.querySelector(".slide").addEventListener('touchstart', handleTouchStart, false);        
+// document.querySelector(".slide").addEventListener('touchmove', handleTouchMove, false);
 
-var xDown = null;                                                        
-var yDown = null;
+// var xDown = null;                                                        
+// var yDown = null;
 
-function getTouches(evt) {
-  return evt.touches             // browser API
+// function getTouches(evt) {
+//   return evt.touches             // browser API
          
-}                                                     
+// }                                                     
               
 
-function handleTouchStart(evt) {
-    const firstTouch = getTouches(evt)[0];                                      
-    xDown = firstTouch.clientX;                                      
-    yDown = firstTouch.clientY;                                      
-};                                                
+// function handleTouchStart(evt) {
+//     const firstTouch = getTouches(evt)[0];                                      
+//     xDown = firstTouch.clientX;                                      
+//     yDown = firstTouch.clientY;                                      
+// };                                                
                                                                          
-function handleTouchMove(evt) {
-    if ( ! xDown || ! yDown ) {
-        return;
-    }
+// function handleTouchMove(evt) {
+//     if ( ! xDown || ! yDown ) {
+//         return;
+//     }
 
-    var xUp = evt.touches[0].clientX;                                    
-    var yUp = evt.touches[0].clientY;
+//     var xUp = evt.touches[0].clientX;                                    
+//     var yUp = evt.touches[0].clientY;
 
-    var xDiff = xDown - xUp;
-    var yDiff = yDown - yUp;
+//     var xDiff = xDown - xUp;
+//     var yDiff = yDown - yUp;
                                                                          
-    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
-        if ( xDiff > 0 ) {
-            /* right swipe */ 
-        } else {
-            /* left swipe */
-        }                       
-    } else {
-        if ( yDiff > 0 ) {
-            document.querySelector(".slide").style.backgroundColor = "red"
-        } else { 
-            document.querySelector(".slide").style.backgroundColor = "white"
-        }                                                                 
-    }
-    /* reset values */
-    xDown = null;
-    yDown = null;                                             
-};
+//     if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+//         if ( xDiff > 0 ) {
+//             /* right swipe */ 
+//         } else {
+//             /* left swipe */
+//         }                       
+//     } else {
+//         if ( yDiff > 0 ) {
+//             document.querySelector(".slide").style.backgroundColor = "red"
+//         } else { 
+//             document.querySelector(".slide").style.backgroundColor = "white"
+//         }                                                                 
+//     }
+//     /* reset values */
+//     xDown = null;
+//     yDown = null;                                             
+// };
